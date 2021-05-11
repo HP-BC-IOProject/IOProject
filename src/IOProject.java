e5b38eaf03935fd9b2b6e9ef846025d9cc7338bb
Brett Crawford <brett.crawford57@gmail.com>
2021-05-11 12:36:25 -0500

    created path to contacts.txt and printed

--- src/IOProject.java
+++ src/IOProject.java
@@ -1,6 +1,15 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.nio.file.Files.exists;
+
 public class IOProject {
     
-    public static void printContacts(Path filePath) throws IOProject {
+    public static void printContacts(Path filePath) throws IOException {
         System.out.println();
         List<String> fileContent  = Files.readAllLines(filePath);
         for (int i = 0; i < fileContent.size(); i++) {
@@ -8,25 +17,35 @@ public class IOProject {
         }
     }
 
-    public static void main(String[] args) throws IOProject {
-        
-        Path PathtoContacts = Paths.get("./src/contacts.txt");
+    public static void main(String[] args) throws IOException {
+
+        String contactDirectory = "./src/";
+        String contactFile = "contacts.txt";
+
+        Path PathtoContacts = Paths.get(contactDirectory, contactFile);
         // Prints out file path
         System.out.println("PathtoContacts = " + PathtoContacts);
-        files.exists(PathtoContacts) System.out.println("PathtoContacts = " + PathtoContacts);
-        
-        String filename = "contacts.txt";
+        Files.exists(PathtoContacts);
 
-        Path contactPath = Paths.get(directory, filename);
+        List<String> newContacts = Arrays.asList("Jeff", "Harry", "Bob"); //Making the list!
+        System.out.println("newContacts = " + newContacts);
 
-        List<String> contacts = Arrays.asList("Brett", "Hunter", "Kenneth", "Douglas", "Larry")
-        System.out.println("contacts = " + contacts);
+        Files.write(PathtoContacts, newContacts);
 
-        Files.write(contactPath, contacts);
+//    System.out.println("PathtoContacts = " + PathtoContacts);
+//
+//        String filename = "contacts.txt";
+//
+//        Path contactPath = Paths.get(filename);
+//
+//        List<String> contacts = Arrays.asList("Brett", "Hunter", "Kenneth", "Douglas", "Larry");
+//        System.out.println("contacts = " + contacts);
+//
+//        Files.write(contactPath, contacts);
+//
+//        System.out.println();
 
-        System.out.println();
-
-        System.out.println("Do you want to add a person?")
+//        System.out.println("Do you want to add a person?");
 
 
 
